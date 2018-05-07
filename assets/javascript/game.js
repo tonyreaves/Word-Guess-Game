00ab9dea775eaf108c7497b8cbdff80f459cab7b
tonyreaves <tonyfreaves@gmail.com>
2018-05-07 00:34:10 -0700

    theme

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+
+
+var randomVillain = Math.floor( Math.random() * 16);
+var villains = ["Apocalypse", "Sauron", "Juggernaut", "Mystique", "Stryfe", "Omega Red", "Master Mold", "Mojo", "Mr Sinister", "Magneto", "Dark Phoenix", "Nimrod", "Graydon Creed", "Blob", "Shadow King", "Sabretooth"]
+
